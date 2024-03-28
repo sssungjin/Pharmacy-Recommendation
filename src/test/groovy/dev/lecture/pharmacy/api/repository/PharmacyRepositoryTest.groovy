9ab94deb98d6744e0cbf39c77e611188bb9137d8
sssungjin <dkdlwls576@gmail.com>
2024-03-28 17:41:42 +0900

    feat: repository 통합 테스트 환경 및 테스트 케이스 생성

--- src/test/groovy/dev/lecture/pharmacy/api/repository/PharmacyRepositoryTest.groovy
+++ src/test/groovy/dev/lecture/pharmacy/api/repository/PharmacyRepositoryTest.groovy
@@ -1,16 +1,36 @@
 package dev.lecture.pharmacy.api.repository
 
+import dev.lecture.pharmacy.api.AbstractIntegrationContainerBaseTest
+import dev.lecture.pharmacy.api.entity.Pharmacy
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
-import spock.lang.Specification
 
-@SpringBootTest
-class PharmacyRepositoryTest extends Specification {
+class PharmacyRepositoryTest extends AbstractIntegrationContainerBaseTest {
 
     @Autowired
     private PharmacyRepository pharmacyRepository
 
-    def "test"() {
+    def "PharmacyRepository save"() {
         // pharmacyRepository
+        given:
+        String address = "서울 특별시 성북구 종암동"
+        String name = "은혜 약국"
+        double latitude = 36.11
+        double longitude = 128.11
+
+        def pharmacy = Pharmacy.builder()
+            .pharmacyAddress(address)
+            .pharmacyName(name)
+            .latitude(latitude)
+            .longitude(longitude)
+            .build()
+
+        when:
+        def result = pharmacyRepository.save(pharmacy)
+
+        then:
+        result.getPharmacyAddress() == address
+        result.getPharmacyName() == name
+        result.getLatitude() == latitude
+        result.getLongitude() == longitude
     }
 }
