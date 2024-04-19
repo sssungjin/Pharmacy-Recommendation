4e73927a7e26edbde469e70a2675a4d97a7f4eb6
sssungjin <dkdlwls576@gmail.com>
2024-04-19 18:33:55 +0900

    feat: 로드뷰, 지도 링크 추기

--- src/main/java/dev/lecture/pharmacy/service/PharmacyRecommendationService.java
+++ src/main/java/dev/lecture/pharmacy/service/PharmacyRecommendationService.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.Collections;
 import java.util.List;
@@ -24,6 +25,9 @@ public class PharmacyRecommendationService {
     private final KakaoAddressSearchService kakaoAddressSearchService;
     private final DirectionService directionService;
 
+    private static final String ROAD_VIEW_BASE_URL = "https://map.kakao.com/link/roadview/";
+    private static final String DIRECTION_BASE_URL = "https://map.kakao.com/link/map/";
+
     public List<OutputDto> recommendPharmacyList(String address) {
 
         KakaoApiResponseDto kakaoApiResponseDto = kakaoAddressSearchService.requestAddressSearch(address);
@@ -35,7 +39,8 @@ public class PharmacyRecommendationService {
 
         DocumentDto documentDto = kakaoApiResponseDto.getDocumentList().get(0);
 
-        //List<Direction> directionList = directionService.buildDirectionListByCategoryApi(documentDto);
+        // kakao 카테고리를 이용한 장소 검색 api 이용
+        // List<Direction> directionList = directionService.buildDirectionListByCategoryApi(documentDto);
         List<Direction> directionList = directionService.buildDirectionList(documentDto);
 
         return directionService.saveAll(directionList)
@@ -45,11 +50,20 @@ public class PharmacyRecommendationService {
     }
 
     private OutputDto converToOutputDto(Direction direction) {
+
+        String params = String.join(",",direction.getTargetPharmacyName(),
+                String.valueOf(direction.getTargetLatitude()), String.valueOf(direction.getTargetLongitude()));
+
+        String result = UriComponentsBuilder.fromHttpUrl(DIRECTION_BASE_URL + params)
+                .toUriString();
+
+        log.info("direction params: {}, url: {}", params, result);
+
         return OutputDto.builder()
                 .pharmacyName(direction.getTargetPharmacyName())
                 .pharmacyAddress(direction.getTargetAddress())
-                .directionUrl("todo")
-                .roadViewUrl("todo")
+                .directionUrl(result)
+                .roadViewUrl(ROAD_VIEW_BASE_URL + direction.getTargetLatitude() + "," + direction.getTargetLongitude())
                 .distance(String.format("%.2f km", direction.getDistance()))
                 .build();
     }
